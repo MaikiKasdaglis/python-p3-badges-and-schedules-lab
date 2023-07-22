ae1d3693da7d911bed02bc2566f2b9a1ff44fe62
Michael Kasdaglis <maikikasdaglis@gmail.com>
2023-07-22 14:12:24 -0600

    finishes lab

--- lib/conference_badges.py
+++ lib/conference_badges.py
@@ -1,11 +1,28 @@
 def badge_maker(name):
-    return None
-
+    badge = f"Hello, my name is {name}."
+    return badge
+    
 def batch_badge_creator(names):
-    return None
+    badges = []
+    for name in names:
+        badge = badge_maker(name)
+        badges.append(badge)
+    return badges
 
 def assign_rooms(names):
-    return None
+    room_assignments = []
+    for i in range(len(names)):
+        room_number = i + 1
+        assignment = f"Hello, {names[i]}! You'll be assigned to room {room_number}!"
+        room_assignments.append(assignment)
+    return room_assignments
 
 def printer(names):
-    return None
+    badges = batch_badge_creator(names)
+    assignments = assign_rooms(names)
+
+    for badge in badges:
+        print(badge)
+
+    for assignment in assignments:
+        print(assignment)
